7fe05479217f8f1608284a38e0a90801bfc66028
Pranjal Siraskar <pranjal.siraskar17@vit.edu>
2021-02-28 20:07:34 +0530

    Cleared all simple test cases of empty string, 1 number and 2 number

--- src/prs/StringCalculator.java
+++ src/prs/StringCalculator.java
@@ -4,6 +4,9 @@ public class StringCalculator {
     public static int add(String numbers){
         if(numbers.isEmpty())
             return 0;
-        return Integer.parseInt(numbers);
+        String[] numarray=numbers.split(",");
+        if(numarray.length==1)
+            return Integer.parseInt(numarray[0]);
+        return Integer.parseInt(numarray[0])+Integer.parseInt(numarray[1]);
     }
 }
