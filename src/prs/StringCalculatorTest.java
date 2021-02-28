7d6681fa7d9059c0ca847560b0bf18a044799ff8
Pranjal Siraskar <pranjal.siraskar17@vit.edu>
2021-02-28 17:13:18 +0530

    Empty String test case created

--- src/prs/StringCalculatorTest.java
+++ src/prs/StringCalculatorTest.java
@@ -1,5 +1,12 @@
 package prs;
 
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class StringCalculatorTest {
-    
+    @Test
+    public void EmptyString(){
+        assertThat("", Calculator.add(""));
+    }
 }
