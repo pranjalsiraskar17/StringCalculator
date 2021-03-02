cfcfe706c3077f7c25f0fa5a04029f90c1dcc2e3
Pranjal Siraskar <pranjal.siraskar17@vit.edu>
2021-03-02 15:05:38 +0530

    Code refactored- findNumbers function updated

--- src/prs/StringCalculator.java
+++ src/prs/StringCalculator.java
@@ -1,39 +1,33 @@
 package prs;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public static int add(String numbers) throws Exception {
         if(numbers.isEmpty())
             return 0;
-        String[] numarray=findNumbers(numbers);
+        int[] numarray=findNumbers(numbers);
         int sum=arrayAdd(numarray);
         return sum; 
     }
 
-    public static int stringToInt(String str) {
-        return Integer.parseInt(str);
-    }
-
-    public static String[] findNumbers(String numbers) {
+    public static int[] findNumbers(String numbers) {
         String delimiter;
-        String[] numarray;
         if(numbers.matches("//(.*)\n(.*)")){
             delimiter=numbers.charAt(2)+"|\n";
-            String sub=numbers.substring(4);
-            numarray = sub.split(delimiter);
+            return Arrays.stream(numbers.substring(4).split(delimiter)).mapToInt(Integer::parseInt).toArray();
         }
         else{
             delimiter=",|\n";
-            numarray = numbers.split(delimiter);
         }
-        return numarray;
+        return  Arrays.stream(numbers.split(delimiter)).mapToInt(Integer::parseInt).toArray();
     }
 
-    public static int arrayAdd(String[] numarray) throws Exception {
+    public static int arrayAdd(int[] numarray) throws Exception {
         int sum=0;
         boolean bool=false;
         String msg="";
-        for(String str:numarray){
-            int temp=stringToInt(str);
+        for(int temp:numarray){
             if(temp<0){
                 bool=true;
                 if(msg.isEmpty())
